dc2e35eb6e7f55bc332df9fe0b7d718c2282d320
dawoyo <111173354+dawoyo@users.noreply.github.com>
2023-06-29 01:18:09 -0600

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://18.224.60.101:9000/',
+  serverUrl: 'http://15.223.56.192:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
